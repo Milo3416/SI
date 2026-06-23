d4709d7980e2b16888b8361e85a84c72bb8c5777
Camilo Serratos Martínez <132173130+Milo3416@users.noreply.github.com>
2026-06-22 23:52:44 -0600

    v2

---
+++ js/genesys-config.js
@@ -0,0 +1,18 @@
+// Genesys Cloud configuration for UP Si Vale demo.
+// Messenger deployment provided by Camilo Serratos.
+window.SIVALE_GENESYS = {
+  enabled: true,
+  deploymentId: '3ef5b987-e978-4d21-a6da-9562d5eebb9d',
+  environment: 'prod',
+  scriptUrl: 'https://apps.mypurecloud.com/genesys-bootstrap/genesys.min.js',
+  demoName: 'up-sivale-digital-tracking-demo',
+  brand: 'Up Si Vale',
+  inactivitySeconds: 15,
+  openMessengerOnAccept: true,
+  showInviteOnAllPages: true,
+  showInviteOnKnowledgePortal: true,
+  inviteMode: 'center',
+  searchQueryParam: 'q',
+  knowledgeBaseName: 'MEX_CS_SIVALE',
+  debug: true
+};
